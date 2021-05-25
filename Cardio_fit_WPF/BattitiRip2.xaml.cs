72f3012e72cacf6a6e8f2279fdf333e9ce925535
Grupp-Assiurd <83281986+Grupp-Assiurd@users.noreply.github.com>
2021-05-25 13:32:07 +0200

    Add files via upload

---
+++ Cardio_fit_WPF/BattitiRip2.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using CardioLibrary;
+
+namespace Cardio_fit_WPF
+{
+    /// <summary>
+    /// Logica di interazione per BattitiRip2.xaml
+    /// </summary>
+    public partial class BattitiRip2 : Window
+    {
+        public BattitiRip2()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_cerca_Click(object sender, RoutedEventArgs e)
+        {
+            bool risposta = false;
+            if (int.Parse(txt_battitoDaRicercare.Text) >= 60 && int.Parse(txt_battitoDaRicercare.Text) <= 100)
+            {
+                risposta = DataCardio.BattitiRiposoFile(int.Parse(txt_battitoDaRicercare.Text));
+            }
+            else
+            {
+                MessageBox.Show("attenzione inserire un valore compreso tra 60 e 100", "attenzione", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            if (risposta)
+                lbl_risultatoRicerca.Content = "il battito inserito è presente nel file";
+            else
+                lbl_risultatoRicerca.Content = "il battito inserito non è presente nel file";
+
+
+        }
+    }
+}
